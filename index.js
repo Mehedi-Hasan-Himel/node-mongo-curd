28b9824bda433553ce2d77b801090e0871684a67
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-01 10:35:55 +0600

    Send data to database using mongodb insertOne

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(cors())
+// use middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.send('running my node curd server')
+// user: dbuser1
+// pass: vigP8GwlydQWZVT5
+
+const uri =
+  "mongodb+srv://dbuser1:vigP8GwlydQWZVT5@cluster0.rky6sdd.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("user");
+    const user = { name: "Mehedi Hasan Himel", email: "mehedihasanhimel89@gmail.com" };
+    const result = await userCollection.insertOne(user);
+    console.log(`User inserted with id: ${result.insertedId}`);
+  } finally {
+    // await client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("running my node curd server");
 });
 
 app.listen(port, () => {
-   console.log('CURD Server is running');
-})
+  console.log("CURD Server is running");
+});
