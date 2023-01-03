bc7da8adab408c6574fd56c739548980c6ea59c3
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-04 01:45:19 +0600

    data send to mongoDB database

--- index.js
+++ index.js
@@ -24,11 +24,13 @@ async function run() {
     await client.connect();
     const userCollection = client.db("foodExpress").collection("user");
 
-    app.post("/user", (req, res) => {
+    app.post("/user", async(req, res) => {
       const newUser = req.body;
       console.log("adding new user", newUser);
-      res.send({result:'success'});
+      const result =  await userCollection.insertOne(newUser)
+      res.send(result);
     });
+
   } finally {
     // await client.close()
   }
