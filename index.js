72d75664380aaa874dde39387a3d613ea90c35b2
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-04 21:07:45 +0600

    update data front end to backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const ObjectId = require('mongodb').ObjectId
+const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,37 +26,71 @@ async function run() {
     const userCollection = client.db("foodExpress").collection("user");
 
     // get users
-    app.get('/user', async(req, res) => {
-      const query = {}
-      const cursor = userCollection.find(query)
-      const users = await cursor.toArray()
-      res.send(users)
-    })
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
 
-    // 
+    //
     app.get(`/user/:id`, async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) }
-      const result = await userCollection.findOne(query)
-      res.send(result)
-    })
-
-    // POST User : Add a new user.
-    app.post("/user", async(req, res) => {
-      const newUser = req.body;
-      console.log("adding new user", newUser);
-      const result =  await userCollection.insertOne(newUser)
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query);
       res.send(result);
     });
 
-    // delete a user
-    app.delete('/user/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id) };
-      const result = await userCollection.deleteOne(query) 
-      res.send(result)
-    })
+    // POST User : Add a new user.
+    app.post("/user", async (req, res) => {
+      const newUser = req.body;
+      console.log("adding new user", newUser);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
 
+    // update user
+    app.put(`/user/:id`, async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upset: true };
+      const updateDoc = {
+        set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    // Update user
+    app.put(`/user/:id`, async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: objectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+        },
+      };
+      const resutl = await userCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+    });
+
+    // delete a user
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close()
   }
